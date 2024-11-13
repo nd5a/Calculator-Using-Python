a36ffe99d815c05e333df134f07f39d80fac1914
Dhruvil Nakrani <128470872+nd5a@users.noreply.github.com>
2024-11-13 17:36:50 +0530

    Calculator

---
+++ Calculator.py
@@ -0,0 +1,78 @@
+# Calculator
+import os
+
+def clear():
+    # Clear the console for cross-platform use
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+print('''
+ _____________________
+|  _________________  |
+| | DN-Python    0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|                                         
+                                     ''')   
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    if n2 == 0:
+        return "Error! Division by zero is not allowed"
+    else:
+        return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+def calculator():
+    num1 = float(input("Enter the first number: "))
+
+    for symbol in operations:
+        print(symbol)
+
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Enter an operation from the line above: ")
+        if operation_symbol not in operations:
+            print("Invalid operation. Please choose a valid operation.")
+            continue
+
+        num2 = float(input("Enter the second number: "))
+
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        
+        user_choice = input(f"Type 'y' to continue calculating with {answer}, 'n' to start a new calculation, or 'e' to exit: ").lower()
+        if user_choice == "y":
+            num1 = answer
+        elif user_choice == "e":
+            should_continue = False
+        else:
+            should_continue = False
+            clear()
+            calculator()
+
+calculator()
