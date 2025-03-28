1839c53389c335e746cb8d9a3b2cb835cee40b3d
Dhruvil Nakrani <128470872+nd5a@users.noreply.github.com>
2025-03-28 10:14:23 +0530

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'http://localhost:4000/api/v1'
+// export const BASE_URL = 'http://localhost:4000/api/v1'
 // export const BASE_URL = 'http://mern-travel-booking-api.vercel.app/'
+export const BASE_URL = 'https://mona-voyaje-travel-app-api.onrender.com'
